998be2ca6a4e995a36f21b793eda81eb05b37717
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-11 22:08:44 +0530

    Fix indentation in Bubble Sort implementation

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,8 +1,7 @@
 arr = [5, 3, 8, 4, 2]
-
 for i in range(len(arr)):
     for j in range(len(arr)-i-1):
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
-
 print("Sorted List:", arr)
+
